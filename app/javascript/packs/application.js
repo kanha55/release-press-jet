e28556b61a30bba1c8497c274bcfad3aaccdd93c
kanha <kanha.dubey9@gmail.com>
2021-10-19 13:39:46 +0530

    Copy to clipborad and other fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
+//= require clipboard
 import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -12,6 +12,7 @@ require("channels")
 
 window.jQuery = $;
 window.$ = $;
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
